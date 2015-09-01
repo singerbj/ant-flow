f8bd2bf48331b326aea3fdc3833f640c31307bc8
Ben Singer <benjamin.singer@helpsystems.com>
2015-09-01 15:26:03 -0500

    modularized a bit

--- myjs.js
+++ myjs.js
@@ -26,72 +26,7 @@ app.controller('Controller', ["$scope", "$http", function($scope, $http) {
             var file = element.files[0];
             var reader = new FileReader();
             reader.onload = function(e) {
-                var xml = e.target.result.toString();
-                var x2js = new X2JS();
-                var json = x2js.xml_str2json(xml);
-
-                var allTargets = {};
-                var props = {};
-                json.project.target.forEach(function(target) {
-                    var targetObj = {};
-                    targetObj.name = target._name.trim();
-                    if (target._description) {
-                        targetObj.description = target._description.trim() ? target._description : '';
-                    }
-                    if (target.antcall && target.antcall.length) {
-                        targetObj.antcalls = target.antcall.map(function(antcall) {
-                            return antcall._target.trim();
-                        });
-                    } else if (target.antcall && !target.antcall.length) {
-                        targetObj.antcalls = [target.antcall._target.trim()];
-                    } else {
-                        targetObj.antcalls = [];
-                    }
-                    if (target._depends) {
-                        targetObj.depends = target._depends.replace(/ /g, '').split(',');
-                    } else {
-                        targetObj.depends = [];
-                    }
-                    if (target._if) {
-                        targetObj.if = target._if.replace(/ /g, '');
-                        if (!props[targetObj.if]) {
-                            props[targetObj.if] = ({
-                                name: targetObj.if,
-                                selected: true
-                            });
-                        }
-                    }
-                    if (target._unless) {
-                        targetObj.unless = target._unless.replace(/ /g, '');
-                        if (!props[targetObj.unless]) {
-                            props[targetObj.unless] = {
-                                name: targetObj.unless,
-                                selected: true
-                            };
-                        }
-                    }
-                    targetObj.contents = {};
-                    for (var key in target) {
-                        if (target.hasOwnProperty(key) && key != '$' && key != 'antcall') {
-                            targetObj.contents[key] = target[key];
-                        }
-                    }
-                    allTargets[target._name] = targetObj;
-                });
-
-                self.targets = allTargets;
-                self.props = props;
-                self.xml = xml;
-                for (var key in self.targets) {
-                    if (self.targets.hasOwnProperty(key)) {
-                        self.targetKeys.push(key);
-                    }
-                }
-                self.targetKeys.sort();
-                self.baseTarget = self.targetKeys[0];
-                self.update();
-                self.loading = false;
-                $scope.$apply();
+                self.convert(e.target.result.toString());
             };
             if (file) {
                 self.fileChosen = true;
@@ -100,6 +35,73 @@ app.controller('Controller', ["$scope", "$http", function($scope, $http) {
                 self.fileChosen = false;
             }
         });
+    };
+
+    self.convert = function(xml){
+        var x2js = new X2JS();
+        var json = x2js.xml_str2json(xml);
+        var allTargets = {};
+        var props = {};
+        json.project.target.forEach(function(target) {
+            var targetObj = {};
+            targetObj.name = target._name.trim();
+            if (target._description) {
+                targetObj.description = target._description.trim() ? target._description : '';
+            }
+            if (target.antcall && target.antcall.length) {
+                targetObj.antcalls = target.antcall.map(function(antcall) {
+                    return antcall._target.trim();
+                });
+            } else if (target.antcall && !target.antcall.length) {
+                targetObj.antcalls = [target.antcall._target.trim()];
+            } else {
+                targetObj.antcalls = [];
+            }
+            if (target._depends) {
+                targetObj.depends = target._depends.replace(/ /g, '').split(',');
+            } else {
+                targetObj.depends = [];
+            }
+            if (target._if) {
+                targetObj.if = target._if.replace(/ /g, '');
+                if (!props[targetObj.if]) {
+                    props[targetObj.if] = ({
+                        name: targetObj.if,
+                        selected: true
+                    });
+                }
+            }
+            if (target._unless) {
+                targetObj.unless = target._unless.replace(/ /g, '');
+                if (!props[targetObj.unless]) {
+                    props[targetObj.unless] = {
+                        name: targetObj.unless,
+                        selected: true
+                    };
+                }
+            }
+            targetObj.contents = {};
+            for (var key in target) {
+                if (target.hasOwnProperty(key) && key != '$' && key != 'antcall') {
+                    targetObj.contents[key] = target[key];
+                }
+            }
+            allTargets[target._name] = targetObj;
+        });
+
+        self.targets = allTargets;
+        self.props = props;
+        self.xml = xml;
+        for (var key in self.targets) {
+            if (self.targets.hasOwnProperty(key)) {
+                self.targetKeys.push(key);
+            }
+        }
+        self.targetKeys.sort();
+        self.baseTarget = self.targetKeys[0];
+        self.update();
+        self.loading = false;
+        $scope.$apply();
     };
 
 
